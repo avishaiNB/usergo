71a559a8c4bcd8856b7b59d5a26eccaddf3518d4
Guy Kolbis <guyk@net-bet.net>
2020-10-03 12:08:47 +0300

    changes in the names

--- user/api.go
+++
@@ -1,8 +0,0 @@
-package user
-
-import "context"
-
-// Service ...
-type Service interface {
-	GetUserByID(ctx context.Context, userID int) (User, error)
-}
--- user/user.go
+++
@@ -1,9 +0,0 @@
-package user
-
-// User ...
-type User struct {
-	ID        int
-	Email     string
-	FirstName string
-	LastName  string
-}
